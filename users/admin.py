ee998b737d935b9126f66aaea3fffba609d4af9c
seungwooj <seungwoo.aaron@gmail.com>
2021-06-26 16:45:00 +0900

    created users application

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
 
 # Register your models here.
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    
+    """ Custom User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                ),
+            },
+        ),
+    )
+
+    # admin.site.register(models.User, CustomUserAdmin)
